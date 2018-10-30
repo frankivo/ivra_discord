2e8aac15f3cae96f39e33a4baf7f953d221a3cfe
Neil Middleton <nmiddleton@salesforce.com>
2018-10-30 17:29:44 +0000

    Car mentions

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content.startsWith(`${process.env.PREFIX}rc`)) {
-		raceControlTextChannel.send(`@here ${message.author} sent a message: \`\`\`${message.content}\`\`\``);
-		message.channel.send('Your message has been sent to Race Control. They will reach out to you shortly if needed.');
+	if (message.content.startsWith(`${process.env.PREFIX}`)) {
+
+		// Race Control notifications
+		if (message.content.startsWith(`${process.env.PREFIX}rc`)) {
+			raceControlTextChannel.send(`@here ${message.author} sent a message: \n\n${message.content}`);
+			message.channel.send('Your message has been sent to Race Control. They will reach out to you shortly if needed.');
+		}
+
+		// Team text notifications by car number(e.g !289)
+		else if (message.member.roles.find("name", process.env.RC_ROLE_NAME)) {
+			const carNumber = message.content.match(/\d+/)[0];
+			const voiceChannel = client.channels.find(item => item.name.match(/\d+/) && item.name.match(/\d+/)[0] === carNumber);
+
+			if (voiceChannel === null) {
+				message.channel.send(`No text channel found for car #${carNumber}`);
+			} else {
+				const categoryId = voiceChannel.parentID;
+				const textChannel = client.channels.find(item => item.parentID === categoryId && item.name === 'text');
+				textChannel.send((`@here ${message.author} sent a message: \n\n${message.content}`))
+			}
+		}
 	}
 });
 
